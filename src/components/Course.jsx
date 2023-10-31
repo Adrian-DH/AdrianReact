20a4913eefb61a16497e1bfe4e6b2e5b919482b0
Adrian Hoffer <adrianhoffer35@gmail.com>
2023-10-31 14:43:25 -0500

    Fall course cy

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -16,20 +16,20 @@ const Course= ({ id, course, selected, toggleSelected}) =>{
     const [profile, profileLoading, profileError] = useProfile();
     const redHighlight = CompatibleClasses(course, selected);
     return (
-    <div key = {id} className="course-card" onClick={() => toggleSelected(course)}>
+    <div key = {id} className="course-card" data-cy="course" onClick={() => toggleSelected(course)}>
     <Card  bg="Light" border="secondary" style={{ width: '18em'}}>
-    <div className={`card-body ${selected.includes(course) ? 'selected' : ''} ${redHighlight ? "notComp" : ''}`}>
-    <Card.Body >
-      <Card.Title>{course.term} CS {course.number}</Card.Title>
-      <Card.Text>
-      {course.title}
-      </Card.Text>
-    </Card.Body>
+      <div className={`card-body ${selected.includes(course) ? 'selected' : ''} ${redHighlight ? "notComp" : ''}`}>
+      <Card.Body >
+        <Card.Title>{course.term} CS {course.number}</Card.Title>
+        <Card.Text>
+        {course.title}
+        </Card.Text>
+      </Card.Body>
 
-    <Card.Footer>
-      <small>{course.meets}</small>
-    </Card.Footer>
-    </div>
+      <Card.Footer>
+        <small>{course.meets}</small>
+      </Card.Footer>
+      </div>
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' , marginBottom: '5px'}}>
     {profile.isAdmin ? (<Link to={`/course/${id}/edit`}><Button>Edit</Button></Link>): null}
     </div>
